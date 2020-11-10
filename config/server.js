cf96abc892e3859bdfd34c63499e945dd2b0f65f
Ali Karim <ali.karim@caxtonfx.com>
2020-11-10 11:19:44 +0000

    Initial Setup of Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '662a8e7a723611702700d823efbe900f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f6208217-ac24-4fd5-bbe8-55c0da7e7f3c'
+};
